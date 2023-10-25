569872a43775a942ed691394a273281caff0e783
saied63 <saied.rajab@gmail.com>
2023-10-25 19:26:10 +0330

    ..

--- syntax/syntax.go
+++ syntax/syntax.go
@@ -14,11 +14,6 @@ func AppendTwoIntSlice(slice1 []int, slice2 []int) (returnslice []int) {
 	return returnslice
 }
 
-func OptimizeSlice( a int[]{...}){
-	targetSlice:=make([]int ,0)
-
-}
-
 func AddSomElemanToSlice(slice []int, a int) []int {
 	slice2 := append(slice, a)
 	fmt.Printf("%d added to slice \n", a)
@@ -27,6 +22,4 @@ func AddSomElemanToSlice(slice []int, a int) []int {
 
 // Copy Slice
 
-
-// for 
-
+// for
