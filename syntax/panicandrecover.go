8046a2f752472186fca54f7611e9ac6d6ab2189e
saied63 <saied.rajab@gmail.com>
2023-11-07 11:16:10 +0330

    qw

--- syntax/panicandrecover.go
+++ syntax/panicandrecover.go
@@ -5,8 +5,20 @@ import (
 )
 
 func TestPanic(a int) {
+	deferRecoverPanic(a)
+	fmt.Println("print after recover panic")
+}
+
+func deferRecoverPanic(a int) {
+	defer recoverPanic()
 	if a == 0 {
 		panic("panic error")
 	}
-	fmt.Println("print after panic")
+	fmt.Println("print right after panic")
+}
+
+func recoverPanic() {
+	if r := recover(); r != nil {
+		fmt.Println("panic recovered")
+	}
 }
