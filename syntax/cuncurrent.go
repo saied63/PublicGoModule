b18bcddb6c87ccd51c62cb1c0629f0503f69cab9
saied63 <saied.rajab@gmail.com>
2023-11-07 18:22:33 +0330

    rtfd

--- syntax/cuncurrent.go
+++ syntax/cuncurrent.go
@@ -7,3 +7,6 @@ import (
 func PrintThisFromCuncurrent(s string) {
 	fmt.Println("i am printing this from concurrent : " + s)
 }
+func AddAnotherToConcurrent(s string) {
+	fmt.Println("i am secound print from concurrent")
+}
