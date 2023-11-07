0d1f04e996b977203d9c33009bb0df39cbbc0ac6
saied63 <saied.rajab@gmail.com>
2023-11-07 19:12:55 +0330

    sss

--- syntax/cuncurrent.go
+++ syntax/cuncurrent.go
@@ -8,5 +8,5 @@ func PrintThisFromCuncurrent(s string) {
 	fmt.Println("i am printing this from concurrent : " + s)
 }
 func AddAnotherToConcurrent(s string) {
-	fmt.Println("i am secound print from concurrent")
+	fmt.Println("i am secound print from concurrent : " + s)
 }
