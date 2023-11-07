e7a00c5b115b90110c42ee4343841b5bdd993091
saied63 <saied.rajab@gmail.com>
2023-11-07 22:10:19 +0330

    dd

--- syntax/cuncurrent.go
+++ syntax/cuncurrent.go
@@ -2,11 +2,29 @@ package syntax
 
 import (
 	"fmt"
+	"time"
 )
 
-func PrintThisFromCuncurrent(s string) {
-	fmt.Println("i am printing this from concurrent : " + s)
+func TestGoRoutine() {
+	fmt.Println("befor call goroutin 1")
+	///////////////////////
+	go callGoRoutine1()
+	// we can sleep some times for execute callGoRoutine1
+	time.Sleep(1 * time.Second)
+	///////////////////////
+	fmt.Println("after call goRoutin 1")
 }
-func AddAnotherToConcurrent(s string) {
-	fmt.Println("i am secound print from concurrent : " + s)
+
+func callGoRoutine1() {
+	fmt.Println("inside goRoutine 1")
+}
+
+func CreateMultipleGoRoutine() {
+	for i := 0; i < 100; i++ {
+		go execute(i)
+	}
+}
+
+func execute(a int) {
+	fmt.Println(a)
 }
